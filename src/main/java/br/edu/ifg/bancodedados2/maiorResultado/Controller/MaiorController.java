c836a943642aa4ce4933b2bc42c1ce9908b9c7a7
weu-souza <weuller1561@gmail.com>
2023-11-22 17:21:56 -0300

    cross origin

--- src/main/java/br/edu/ifg/bancodedados2/maiorResultado/Controller/MaiorController.java
+++ src/main/java/br/edu/ifg/bancodedados2/maiorResultado/Controller/MaiorController.java
@@ -4,6 +4,7 @@ import br.edu.ifg.bancodedados2.maiorResultado.Model.MaiorDTO;
 import br.edu.ifg.bancodedados2.maiorResultado.Service.MaiorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/maior-valor")
+@CrossOrigin("*")
 public class MaiorController {
     @Autowired
     MaiorService maiorValorService;
